1231ac1168adc2ffe784619c7cc291b07ff88593
tomio5 <tomi530k@gmail.com>
2026-06-19 09:10:48 +0900

    fix: keep event loop alive

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
-const { PeerServer } = require('peer')
+javascriptconst { PeerServer } = require('peer')
 
 const port = process.env.PORT || 9000
 
 const peerServer = PeerServer({
   port,
   path: '/',
+})
+
+peerServer.on('connection', (client) => {
+  console.log('client connected:', client.getId())
 })
 
 console.log(`PeerJS server running on port ${port}`)
